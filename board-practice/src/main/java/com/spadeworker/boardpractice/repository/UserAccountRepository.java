f2fe1111805252b7539474cdd0d8df25bf3b9dbb
pepe6bf <pepe6bf@gmail.com>
2022-11-02 23:28:58 +0900

    프로젝트 초기화
    재학습을 위한 프로젝트 초기화

--- board-practice/src/main/java/com/spadeworker/boardpractice/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spadeworker.boardpractice.repository;
-
-import com.spadeworker.boardpractice.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-}
