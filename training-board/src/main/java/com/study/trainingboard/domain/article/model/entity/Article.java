05a823426f0e5e7de74d9bf1d8b25b7cc2aa6198
pepe6bf <pepe6bf@gmail.com>
2022-12-10 03:13:59 +0900

    refactor: #30 Article Entity Class 수정
    Article Entity Class의 내용을 수정하였다.
    
    - 인덱싱 항목에 다음 요소를 추가
      - hashtag
      - createdAt
      - createdBy
    - content 컬럼 타입을 "TEXT"로 설정

--- training-board/src/main/java/com/study/trainingboard/domain/article/model/entity/Article.java
+++ training-board/src/main/java/com/study/trainingboard/domain/article/model/entity/Article.java
@@ -12,7 +12,10 @@ import javax.persistence.*;
         name = "article",
         indexes = {
                 @Index(columnList = "title"),
-                @Index(columnList = "content")
+                @Index(columnList = "content"),
+                @Index(columnList = "hashtag"),
+                @Index(columnList = "createdAt"),
+                @Index(columnList = "createdBy")
         })
 @Entity
 public class Article {
@@ -23,10 +26,10 @@ public class Article {
     @Column(nullable = false)
     private String title; // 게시글 제목
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;  // 게시글 본문
 
-    @Column(nullable = false)
+    @Column
     private String hashtag;  // 게시글 해시태그
 
     // TODO: meta data column 추가 하기(Audit)
