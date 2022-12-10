e1ad0de3e38fed35ca2cc0a7c3ee83170259cae6
pepe6bf <pepe6bf@gmail.com>
2022-12-11 00:34:00 +0900

    feat: #22 Article & ArticleComment Repository Data REST 설정 추가 및 테스트 코드 작성

--- training-board/src/main/java/com/study/trainingboard/domain/article/repository/ArticleCommentRepository.java
+++ training-board/src/main/java/com/study/trainingboard/domain/article/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.study.trainingboard.domain.article.repository;
 
 import com.study.trainingboard.domain.article.model.entity.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
