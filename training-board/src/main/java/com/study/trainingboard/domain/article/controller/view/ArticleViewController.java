83191315b809d7a559af46f41af24e70d6cabcb6
pepe6bf <pepe6bf@gmail.com>
2022-12-16 03:22:29 +0900

    test: #35 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 명세를 열어보았다.

---
+++ training-board/src/main/java/com/study/trainingboard/domain/article/controller/view/ArticleViewController.java
@@ -0,0 +1,10 @@
+package com.study.trainingboard.domain.article.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleViewController {
+}
---
+++ training-board/src/test/java/com/study/trainingboard/domain/article/controller/view/ArticleViewControllerTest.java
@@ -0,0 +1,76 @@
+package com.study.trainingboard.domain.article.controller.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleViewController.class)
+class ArticleViewControllerTest {
+
+    private final MockMvc mockMvc;
+
+    public ArticleViewControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    void view_get_test() throws Exception {
+        // Given
+
+        // When & Then
+        mockMvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Test
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    void view_detail_page_test() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+        mockMvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Test
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    void view_search_page_test() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+        mockMvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Test
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    void view_hashtag_search_page_test() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+        mockMvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+}
