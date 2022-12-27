f6f549664a5e8a0ba3ae3f1e380cf9b34faee308
pepe6bf <pepe6bf@gmail.com>
2022-12-27 16:13:57 +0900

    conf: #51 Security Config 접근 권한 설정

--- training-board/src/main/java/com/study/trainingboard/global/config/SecurityConfig.java
+++ training-board/src/main/java/com/study/trainingboard/global/config/SecurityConfig.java
@@ -1,8 +1,18 @@
 package com.study.trainingboard.global.config;
 
+import com.study.trainingboard.domain.article.dto.UserAccountDto;
+import com.study.trainingboard.domain.article.dto.security.BoardPrincipal;
+import com.study.trainingboard.domain.article.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +21,37 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin();
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                PathRequest.toStaticResources().atCommonLocations()
+                        ).permitAll()
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                ).formLogin()
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/");
+
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findByEmail(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - email : " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
